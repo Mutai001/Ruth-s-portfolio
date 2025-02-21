c6879683e09c46d5b72f305e52e27643746dc968
mutai001 <omusekim@gmail.com>
2025-02-21 20:09:14 +0300

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar"; // Import the Navbar component
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-   <Navbar />
-    </>
-  )
-}
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="p-4">
+        <h1 className="text-3xl font-bold text-center text-gray-800">
+          Welcome to My App!
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
